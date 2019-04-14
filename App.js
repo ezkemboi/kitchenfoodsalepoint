50b46611aaed81560ec1c369536ff601d0b2ad73
me-x-mi <developerezrqn@gmail.com>
2019-04-14 17:43:45 +0300

    add screen

--- App.js
+++ App.js
@@ -6,44 +6,86 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet } from "react-native";
+import { Container, Button, Text } from "native-base";
+import LinearGradient from "react-native-linear-gradient";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Container style={styles.container}>
+        <LinearGradient
+          colors={["#709BA5", "#1A707F"]}
+          style={{ flex: 1, paddingTop: 20 }}
+        >
+          <Button
+            transparent
+            light
+            style={{
+              alignSelf: "center",
+              borderRadius: 15,
+              borderWidth: 2,
+              borderColor: "white",
+              paddingRight: 60,
+              paddingLeft: 60
+            }}
+          >
+            <Text>Menu</Text>
+          </Button>
+          <Text style={styles.welcome}>This is text</Text>
+          <Button
+            transparent
+            light
+            style={{
+              alignSelf: "center",
+              borderRadius: 15,
+              borderWidth: 2,
+              borderColor: "white",
+              paddingRight: 30,
+              paddingLeft: 30,
+              position: "absolute",
+              bottom: 40
+            }}
+          >
+            <Text>Add to Basket</Text>
+          </Button>
+        </LinearGradient>
+      </Container>
     );
   }
 }
 
+// 090d0e   jini   juu 709BA5   ... f6f7f7
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flex: 1
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
   },
+  button: {
+    width: 200
+  }
 });
